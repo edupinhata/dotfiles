a6d7e7e19a48e59b01340012c40a93ebe90a1ef0
EPinhata <edupinhata@gmail.com>
2020-01-31 14:14:44 -0500

    Added i3lock in dotfiles

--- install_my_programs.sh
+++ install_my_programs.sh
@@ -2,4 +2,22 @@
 
 # This is a script to install all programs that I use to work
 
+# Git repositories
+if [ ! -d ~/proj ]; then
+    mkdir ~/proj
+fi
+
+cd ~/proj
+
+if [ ! -d i3lock-fancy ]; then
+    echo "Clonning i3lock-fancy"
+    git clone https://github.com/meskarune/i3lock-fancy.git
+fi
+if [ -d i3lock-fancy ]; then
+    echo "Installing i3lock-fancy"
+    cd i3lock-fancy
+    git checkout dualmonitors
+    sudo make install
+fi
+
 
